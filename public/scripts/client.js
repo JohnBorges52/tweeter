a9a588de19e7819b6b601694a01b661b8ef54984
John <jrborges52@email.com>
2022-06-24 02:15:35 +0000

    eslinted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,57 +3,48 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
-
-  $("#btn-to-tweet").click(function() {
+$(document).ready(function () {
+  $("#btn-to-tweet").click(function () {
     $("#tweet-text").focus();
   });
-  
-  const loadTweets = function() {
-    $.get("http://localhost:8080/tweets").then(data => {
-      renderTweets(data)
-    })
-    
-  }  
-  loadTweets()
-  
-  $(`#new-tweet-form`).on('submit', (event) => {
-    event.preventDefault();
-    
-    const serialized = $(`#new-tweet-form`).serialize();
-    const length = $(`#tweet-text`).val().length
-    if (length > 140) {
-      $(".error-message").css("display", "flex")
-    } else if (length === 0) {
-      alert("You need to type something...")
-    } else {
-      $(".error-message").css("display", "none")
-      $.post("/tweets", serialized);
-      loadTweets()
-      $("#tweet-text").val('')
-      $("#tweet-text").focus()
-    }
 
-  }) 
-  
+  const loadTweets = function () {
+    $.get("http://localhost:8080/tweets").then((data) => {
+      renderTweets(data);
+    });
+  };
+  loadTweets();
+
+  $(`#new-tweet-form`).on("submit", (event) => {
+    event.preventDefault();
+
+    const serialized = $(`#new-tweet-form`).serialize();
+    const length = $(`#tweet-text`).val().length;
+    if (length > 140) {
+      $(".error-message").css("display", "flex");
+    } else if (length === 0) {
+      alert("You need to type something...");
+    } else {
+      $(".error-message").css("display", "none");
+      $.post("/tweets", serialized);
+      loadTweets();
+      $("#tweet-text").val("");
+      $("#tweet-text").focus();
+    }
+  });
 
   const renderTweets = function (tweets) {
-    $( "#tweets-container" ).empty();
-  for (let item of tweets) {
-    const $user = createTweetElement(item)
-    $('#tweets-container').append($user)
-  }
+    $("#tweets-container").empty();
+    for (let item of tweets) {
+      const $user = createTweetElement(item);
+      $("#tweets-container").append($user);
+    }
+  };
 
-}
+  const createTweetElement = function (obj) {
+    const safeHTML = `<p>${escape(obj.content.text)}</p>`;
 
-
-
-const createTweetElement = function(obj) {
-  const safeHTML = `<p>${escape(obj.content.text)}</p>`;
-
-
-  const $tweet = 
-  $(`<article class="tweets-article" id="tweets-container">
+    const $tweet = $(`<article class="tweets-article" id="tweets-container">
   <div class="icon-name-hashtag">
     <div class="icon-name">
       <img class="avatar-pic" src="${obj.user.avatars}" />
@@ -80,15 +71,12 @@ const createTweetElement = function(obj) {
   </footer>
 </article>`);
 
-return $tweet
-}
+    return $tweet;
+  };
 
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
-
-
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 });
-
